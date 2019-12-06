3bf04c068156e6735322462cca98384c03b3f904
careful-principle-0303 <cabaker96@aol.com>
2019-12-06 17:40:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = {railroads: new_hash = {
-    pieces: = "content"
+    pieces: "content"
   }
   }
 end
