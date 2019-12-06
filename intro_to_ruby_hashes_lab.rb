cc242921a94ad27ae4795c04aad7fb5d4b89e8a6
careful-principle-0303 <cabaker96@aol.com>
2019-12-06 19:17:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = {railroads: new_hash = {
-    pieces: 4
+    pieces: 4,
     rent_in_dollars: "test"
     }
   }
