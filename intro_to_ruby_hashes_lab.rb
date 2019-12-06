a1db9be9f1fe00b51a54ea428eb4a99b556e6e96
careful-principle-0303 <cabaker96@aol.com>
2019-12-06 19:23:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,20 +20,21 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  hash = {railroads: new_hash = {
-    pieces: 4,
-    rent_in_dollars: newer_hash = {
-      key1: "stuff",
-      key2: "things",
-      key3: "filler",
-      key4: "content"
-    },
-    names: newest_hash = {
-      key1: "dude",
-      key2: "why",
-      key3: "so many",
-      key4: "keys"
-    }
+  hash = {
+    railroads: new_hash = {
+      pieces: 4,
+      rent_in_dollars: newer_hash = {
+        one_piece_owned: 25,
+        key2: "things",
+        key3: "filler",
+        key4: "content"
+      },
+      names: newest_hash = {
+        key1: "dude",
+        key2: "why",
+        key3: "so many",
+        key4: "keys"
+      }
     }
   }
 end
