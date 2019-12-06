c7fa4e657f674a82341df5ff3a3bb0b61399f79a
careful-principle-0303 <cabaker96@aol.com>
2019-12-06 17:40:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,10 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  hash = {railroads: new_hash = Hash.new}
+  hash = {railroads: new_hash = {
+    pieces: = "content"
+  }
+  }
 end
 
 def monopoly_with_third_tier
