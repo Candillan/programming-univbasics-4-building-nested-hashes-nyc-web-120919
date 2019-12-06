33f4d2f44e7d8686c376b0f4f3b9e065486be81c
careful-principle-0303 <cabaker96@aol.com>
2019-12-06 19:49:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,4 +44,22 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  hash = {
+    railroads: new_hash = {
+      pieces: 4,
+      rent_in_dollars: newer_hash = {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+      },
+      names: newest_hash = {
+        reading_railroad: too_many_hashes = Hash.new,
+        pennsylvania_railroad: but_for_why = Hash.new,
+        b_and_o_railroad: so_many = Hash.new,
+        shortline_railroad: hashes = Hash.new
+      }
+    }
+  }
+end
 end
